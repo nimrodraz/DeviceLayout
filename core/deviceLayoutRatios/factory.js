9edbef65eb7ffdf8cd08e2a26945fdd031ede04f
Nimrod Raz <Nimrod.r@Qiwies.com>
2018-12-20 12:26:16 +0200

    -

--- core/deviceLayoutRatios/factory.js
+++ core/deviceLayoutRatios/factory.js
@@ -1,8 +1,19 @@
 const DeviceLayoutRatios = require('./deviceLayoutRatios.js');
 
 function getDeviceLayoutRatios(){
-    _deviceLayoutRatios =  new DeviceLayoutRatios();
-    _deviceLayoutRatios.loadItems();
+    //return the deviceLayoutRations object containing the configuration of device layout rations
+    var cache = require('memory-cache');
+
+    let _deviceLayoutRatios = cache.get('deviceLayoutRatios');
+    
+    if (_deviceLayoutRatios==null){
+      _deviceLayoutRatios =  new DeviceLayoutRatios();
+      _deviceLayoutRatios.loadItems();  
+      cache.put('deviceLayoutRatios',_deviceLayoutRatios);
+    }
+
+
+
     return _deviceLayoutRatios;
 }
 
