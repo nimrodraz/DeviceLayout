ac43fe365b7e757f0f2f81e2ed356b27898d7fe3
Nimrod Raz <Nimrod.r@Qiwies.com>
2018-12-20 17:41:45 +0200

    -

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const pages = require('./routes/pages')
 
-//allow cors  (accept calls from other domains)
+//allow cors (accept calls from other domains)
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
